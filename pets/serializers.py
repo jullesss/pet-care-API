cd1aa944e0e30464854eba391018c5c729679a66
Leonardo Santos <kuhnnhuk@outlook.com>
2023-06-12 18:04:53 -0300

    feat: task 3 done

--- pets/serializers.py
+++ pets/serializers.py
@@ -1,18 +1,3 @@
-"""
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    first_name = serializers.CharField(max_length=15)
-    last_name = serializers.CharField(max_length=15)
-    email = serializers.EmailField()
-
-    # auto_now_add -> Atualização no momento de criação do dado
-    created_at = serializers.DateTimeField(read_only=True)
-    # auto_now -> Atualização no momento de atualização do dado
-    updated_at = serializers.DateTimeField(read_only=True)
-
-    recipes = RecipeSerializer(many=True, read_only=True)
-"""
-
 from rest_framework import serializers
 from groups.serializers import GroupSerializer
 from traits.serializers import TraitSerializer
--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .models import Pet
+from groups.models import Group
+from traits.models import Trait
+from .serializers import PetSerializer
 
-# Create your views here.
+
+class PetView(APIView):
+    def get(self, request: Request) -> Response:
+        pets = Pet.objects.all()
+
+        serializer = PetSerializer(instance=pets, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request: Request) -> Response:
+        serializer = PetSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        group_data = serializer.validated_data.pop("group")
+        traits_data = serializer.validated_data.pop("traits")
+
+        group_obj = Group.objects.filter(
+            scientific_name__iexact=group_data["scientific_name"]
+        ).first()
+
+        if not group_obj:
+            group_obj = Group.objects.create(**group_data)
+
+        pet = Pet.objects.create(**serializer.validated_data, group=group_obj)
+
+        for trait_dict in traits_data:
+            trait_obj = Trait.objects.filter(name__iexact=trait_dict["name"]).first()
+
+            if not trait_obj:
+                trait_obj = Trait.objects.create(**trait_dict)
+
+            pet.traits.add(trait_obj)
+
+        serializer = PetSerializer(instance=pet)
+
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
